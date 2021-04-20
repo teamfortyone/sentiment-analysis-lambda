1627c56a1645494dd796392814d3ca941b403435
ArionMiles <singh.kanishk07@gmail.com>
2021-04-20 16:32:38 +0530

    Return pos-neg scores directly.
    Also add content-type headers

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -3,39 +3,40 @@ import spacy
 
 loaded_model = spacy.load('/opt/model_artifacts')
 
+HEADERS = {
+    'Access-Control-Allow-Headers': '*',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': '*',
+    'Content-Type': 'application/json',
+}
+
 def lambda_handler(event, context):
     try:
         input_text = event['queryStringParameters']['q']
     except KeyError:
         return {
             'statusCode': 400,
-            'headers': {
-                'Access-Control-Allow-Headers': '*',
-                'Access-Control-Allow-Origin': '*',
-                'Access-Control-Allow-Methods': '*',
-            },
-            'body': 'Please provide input text',
+            'headers': HEADERS,
+            'body': json.dumps({
+                'error': 'Please provide input text'
+            }),
         }
     # Generate prediction    
     parsed_text = loaded_model(input_text)
     # Determine prediction to return
-    if parsed_text.cats['pos'] > parsed_text.cats['neg']:
-        prediction = 'Positive'
-        score = parsed_text.cats['pos']
-    else:
-        prediction = 'Negative'
-        score = parsed_text.cats['neg']
+    # if parsed_text.cats['pos'] > parsed_text.cats['neg']:
+    #     prediction = 'Positive'
+    #     score = parsed_text.cats['pos']
+    # else:
+    #     prediction = 'Negative'
+    #     score = parsed_text.cats['neg']
 
     response = {
         'statusCode': 200,
-        'headers': {
-            'Access-Control-Allow-Headers': '*',
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': '*',
-        },
+        'headers': HEADERS,
         'body': json.dumps({
-            'sentiment': prediction,
-            'score': score,
+            'positive': round(parsed_text.cats['pos'], 5),
+            'negative': round(parsed_text.cats['neg'], 5),
         }),
     }
     return response
